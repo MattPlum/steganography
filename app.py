0d959454e493a4053f716aa9d8bb91bc68c8fa74
mattplum <mattdouglee@gmail.com>
2023-04-02 17:30:15 -0400

    added some css and ref the api key

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, send_file
 from PIL import Image
+import openai
 import requests
 import secrets
+import string
+import config 
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -17,21 +20,18 @@ def generate():
     prompt = request.form.get('prompt')
 
     # Generate an image from the prompt using OpenAI API
-    # Replace YOUR_API_KEY with your OpenAI API key
-    response = requests.post(
-        'https://api.openai.com/v1/images/generations',
-        headers={'Authorization': 'Bearer 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'},
-        json={
-            'model': 'image-alpha-001',
-            'prompt': prompt,
-            'num_images': 1,
-            'size': '256x256',
-            'response_format': 'url'
-        }
+    openai.api_key = config.OPENAI_API_KEY
+
+    # Create an OpenAI DALL-E image
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="256x256",
+        response_format="url"
     )
 
     # Get the URL of the image generated from the API 
-    image_url = response.json()['data'][0]['url']
+    image_url = response['data'][0]['url']
 
     # Load the image from the URL
     image_data = Image.open(requests.get(image_url, stream=True).raw)
@@ -42,15 +42,25 @@ def generate():
     
     # Get the secret message from the user and convert to binary format
     secret_message = request.form.get('secret_message')
-    binary_message = ''.join(format(ord(i), '08b') for i in secret_message)
-
-    # Get the size of the image
+    enc_method = request.form.get('encryption_method')
     width, height = image_data.size
-
-    # Check if the binary message can fit in the image
+    
+    if(enc_method == "none"):
+        binary_message = ''.join(format(ord(i), '08b') for i in secret_message)
+    elif enc_method == "caesar":
+        # Apply the Caesar cipher with a shift of 3
+        ciphertext = caesar_cipher(secret_message, 3)
+        binary_message = ''.join(format(ord(i), '08b') for i in ciphertext)
+        # Get the size of the image
+    elif enc_method == "vigenere":
+        # Apply the Vigenère cipher with a key of "secret"
+        ciphertext = vigenere_cipher(secret_message, "secret")
+        binary_message = ''.join(format(ord(i), '08b') for i in ciphertext)
+        
+   # Check if the binary message can fit in the image
     if len(binary_message) > width * height * 3:
-        raise ValueError("Message is too long to encode in the image")
-
+                raise ValueError("Message is too long to encode in the image")
+    
     # Add padding to the binary message to fill the remaining bits in the image
     binary_message += "0" * (width * height * 3 - len(binary_message))
 
@@ -91,6 +101,25 @@ def generate():
     encoded_image.save('static/encoded_image.png')
 
     return render_template('result.html', image_url=image_url)
+
+def caesar_cipher(plaintext, shift):
+        alphabet = string.ascii_lowercase
+        shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+        table = str.maketrans(alphabet, shifted_alphabet)
+        return plaintext.translate(table)
+
+# Define the Vigenère cipher function
+def vigenere_cipher(plaintext, key):
+    key = key.lower()
+    plaintext = plaintext.lower()
+    key_len = len(key)
+    key_as_int = [ord(i) - ord('a') for i in key]
+    plaintext_int = [ord(i) - ord('a') for i in plaintext]
+    ciphertext = ''
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_as_int[i % key_len]) % 26
+        ciphertext += chr(value + ord('a'))
+    return ciphertext
 
 def decode_message(image_file):
     # Load the encoded image
